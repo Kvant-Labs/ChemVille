9524b22fc38e0bb45ef4a164882f5b497d7017c7
hesengg <heseng@ethz.ch>
2025-04-16 18:26:39 -0400

    updated tmx

--- environment/frontend_server/static_dirs/assets/chemville/visuals/src/Characters.tsx
+++ environment/frontend_server/static_dirs/assets/chemville/visuals/src/Characters.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.1" name="Characters" tilewidth="16" tileheight="16" tilecount="684" columns="19">
- <image source="../Tileset Characters.png" width="304" height="576"/>
+ <image source="../tilesets/Tileset Characters.png" width="304" height="576"/>
 </tileset>
