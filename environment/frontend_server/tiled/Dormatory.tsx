66c96c8f0271236e84f2acbf680bdd520704b149
hesengg <heseng@ethz.ch>
2025-04-18 12:22:43 -0400

    changed main_script.html and hope that it should work now. Also adapted some parts of the tmx

--- environment/frontend_server/tiled/Dormatory.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.1" name="Dormatory" tilewidth="16" tileheight="16" tilecount="400" columns="20">
- <image source="../Tileset Dormatory.png" width="320" height="320"/>
-</tileset>
